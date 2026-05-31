6b1ece90ee1a60ba373f649ba2fb5565dcef170a
Iftekhar Al Mahmud <azim.iftekhar13@gmail.com>
2026-05-31 19:52:19 +1000

    Added user schema

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+
+    role: {
+      type: String,
+      enum: ["visitor", "patient", "doctor", "security", "admin"],
+      required: true
+    },
+
+    birthDate: {
+      type: String,
+      default: ""
+    },
+
+    address: {
+      type: String,
+      default: "",
+      trim: true
+    },
+
+    photo: {
+      type: String,
+      default: ""
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
